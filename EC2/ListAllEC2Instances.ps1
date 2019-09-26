e8596b126d72099951bc4355235d968c4bf4c47a
Rowe <CH212692@CHBOSTON.ORG>
2019-09-26 14:49:06 -0400

    Updated Listall ec2instances
    Now it can list all instances and states on a single account, where you are calling it from.  Leave out the 'role' parameter

--- EC2/ListAllEC2Instances.ps1
+++ EC2/ListAllEC2Instances.ps1
@@ -3,38 +3,82 @@
 
 param
 (
-    [Parameter(Mandatory = $true,
+    [Parameter(Mandatory = $false,
         Position = 1,
-        HelpMessage = 'Supply a stored AWS credential role authorized across accounts')]
-    [Alias('creds')]
+        HelpMessage = 'Supply a stored AWS credential role authorized for cross accounts listing')]
     [System.String]$Role
 )
     
-    $orgs = Get-ORGAccountList
+    
     $regions = Get-AWSRegion
-    $ec2Array = @()
-    $orgProgress = 1
-    $ocount = $orgs.count
     $rcount = $Regions.count
-    $orgs|%{
-        $org = $_
-        $oname = $org.name
-        Write-Progress -Activity "Searching Through Org: $oname" -id 1 -Status "$orgProgress complete of $ocount" -PercentComplete ($orgProgress/$ocount*100)
+    $ec2Array = @()
+    if ($Role){
+        $orgs = Get-ORGAccountList
+        $orgProgress = 1
+        $ocount = $orgs.count
         
-        $regionProgress = 1
-            try{
-                try{$Credentials = Get-STSCreds  -OrganizationID $_.Id -role $role
-                    $regions | % {
+        $orgs|%{
+            $org = $_
+            $oname = $org.name
+            Write-Progress -Activity "Searching Through Org: $oname" -id 1 -Status "$orgProgress complete of $ocount" -PercentComplete ($orgProgress/$ocount*100)
+            
+            $regionProgress = 1
+                try{
+                    try{$Credentials = Get-STSCreds  -OrganizationID $_.Id -role $role
+                        $regions | % {
+                            $oname = $org.name
+                            $region = $_
+                            $rname = $_.name
+                            Write-Progress -Activity "Searching Through REGION: $rname on ORG: $oname" -id 2 -Status "$regionProgress complete of $Rcount"  -PercentComplete ($regionProgress/$Rcount*100) -CurrentOperation "Looking for EC2s in all regions for All Orgs"
+                            
+                            try{$ec2 = Get-EC2Instance -Credential $Credentials -Region $_.Region
+                                $EC2|%{
+                                    Clear-Variable -Name status
+                                    $status = Get-ec2instancestatus -InstanceId $_.instances.instanceid -Region $region.Region -Credential $Credentials
+                                    $Pemkey = $ec2 |select instances
+                                    $obj = new-object psobject
+                    
+                                    $obj |Add-member NoteProperty OrganizationID $org.id
+                                    $obj |Add-member NoteProperty OrganizationName $org.Name
+                                    $obj |Add-member NoteProperty EC2InstanceID $_.instances.instanceid
+                                    $obj |Add-member NoteProperty InstanceType $_.instances.InstanceType
+                                    $obj |Add-member NoteProperty Region $region.Region
+                                    if($status){
+                                        $obj |Add-member NoteProperty InstanceState $status.instancestate.name
+                                        $obj |Add-member NoteProperty Reachability $status.status.status
+                                        $obj |Add-member NoteProperty InstanceStatus $status
+    
+                                    }
+                                    else{
+                                        $obj |Add-member NoteProperty InstanceState "NoStateFound"
+                                        $obj |Add-member NoteProperty Reachability "NoStateFound"
+                                        $obj |Add-member NoteProperty InstanceStatus "NoStateFound"
+                                    }
+                                    $obj |Add-member NoteProperty KeyPem $_.instances
+                                    $ec2Array += $obj
+    
+                                }
+                                
+                            }
+                            catch{}
+                            $regionProgress++
+                        }
+                        }
+                    catch{}
+                #Iterate over all regions and list instances
+                
+                     }
+                     catch{$regions | % {
                         $oname = $org.name
                         $region = $_
                         $rname = $_.name
-                        Write-Progress -Activity "Searching Through REGION: $rname on ORG: $oname" -id 2 -Status "$regionProgress complete of $Rcount"  -PercentComplete ($regionProgress/$Rcount*100) -CurrentOperation "Looking for EC2s in all regions for All Orgs"
-                        
-                        try{$ec2 = Get-EC2Instance -Credential $Credentials -Region $_.Region
+                         Write-Progress -Activity "Searching Through REGION: $rname on ORG: $oname" -id 2 -Status "$regionProgress complete of $Rcount"  -PercentComplete ($regionProgress/$Rcount*100) -CurrentOperation "Looking for EC2s in all regions for All Orgs"
+                        try{$ec2 = Get-EC2Instance -Region $_.Region
                             $EC2|%{
                                 Clear-Variable -Name status
-                                $status = Get-ec2instancestatus -InstanceId $_.instances.instanceid -Region $region.Region -Credential $Credentials
-                                $Pemkey = $ec2 |select instances
+                                $status = Get-ec2instancestatus -InstanceId $_.instances.instanceid -Region $region.Region
+                                $Pemkey = $_ |select instances
                                 $obj = new-object psobject
                 
                                 $obj |Add-member NoteProperty OrganizationID $org.id
@@ -46,65 +90,70 @@ param
                                     $obj |Add-member NoteProperty InstanceState $status.instancestate.name
                                     $obj |Add-member NoteProperty Reachability $status.status.status
                                     $obj |Add-member NoteProperty InstanceStatus $status
-
+    
                                 }
                                 else{
                                     $obj |Add-member NoteProperty InstanceState "NoStateFound"
                                     $obj |Add-member NoteProperty Reachability "NoStateFound"
                                     $obj |Add-member NoteProperty InstanceStatus "NoStateFound"
                                 }
-                                $obj |Add-member NoteProperty KeyPem $_.instances
+                                $obj |Add-member NoteProperty Instances $ec2.instances
                                 $ec2Array += $obj
-
+    
                             }
                             
                         }
                         catch{}
                         $regionProgress++
                     }
-                    }
-                catch{}
-            #Iterate over all regions and list instances
-            
-                 }
-                 catch{$regions | % {
-                     $region = $_
-                     Write-Progress -Activity "Searching Through Orgs" -Status "$regionProgress complete of $Regions.count"  -PercentComplete ($regionProgress/$Regions.count*100) -CurrentOperation InnerLoop
-                    try{$ec2 = Get-EC2Instance -Region $_.Region
-                        $EC2|%{
-                            Clear-Variable -Name status
-                            $status = Get-ec2instancestatus -InstanceId $_.instances.instanceid -Region $region.Region
-                            $Pemkey = $_ |select instances
-                            $obj = new-object psobject
-            
-                            $obj |Add-member NoteProperty OrganizationID $org.id
-                            $obj |Add-member NoteProperty OrganizationName $org.Name
-                            $obj |Add-member NoteProperty EC2InstanceID $_.instances.instanceid
-                            $obj |Add-member NoteProperty InstanceType $_.instances.InstanceType
-                            $obj |Add-member NoteProperty Region $region.Region
-                            if($status){
-                                $obj |Add-member NoteProperty InstanceState $status.instancestate.name
-                                $obj |Add-member NoteProperty Reachability $status.status.status
-                                $obj |Add-member NoteProperty InstanceStatus $status
+                }
+                $orgProgress++
+            }
+        }else{ #NO role Specified, only doing for 1 account
 
-                            }
-                            else{
-                                $obj |Add-member NoteProperty InstanceState "NoStateFound"
-                                $obj |Add-member NoteProperty Reachability "NoStateFound"
-                                $obj |Add-member NoteProperty InstanceStatus "NoStateFound"
-                            }
-                            $obj |Add-member NoteProperty Instances $ec2.instances
-                            $ec2Array += $obj
+            $regions | % {
+                $oname = Get-IAMAccountAlias
+                if (!$oname){$oname = 'NoAccountAlias'}
+                $region = $_
+                $rname = $_.name
+                 Write-Progress -Activity "Searching Through REGION: $rname on ORG: $oname" -id 2 -Status "$regionProgress complete of $Rcount"  -PercentComplete ($regionProgress/$Rcount*100) -CurrentOperation "Looking for EC2s in all regions for All Orgs"
+                try{$ec2 = Get-EC2Instance -Region $_.Region
+                    $EC2|%{
+                        Clear-Variable -Name status
+                        $status = Get-ec2instancestatus -InstanceId $_.instances.instanceid -Region $region.Region
+                        $Pemkey = $_ |select instances
+                        $obj = new-object psobject
+        
+                        $obj |Add-member NoteProperty OrganizationID $org.id
+                        $obj |Add-member NoteProperty OrganizationName $org.Name
+                        $obj |Add-member NoteProperty EC2InstanceID $_.instances.instanceid
+                        $obj |Add-member NoteProperty InstanceType $_.instances.InstanceType
+                        $obj |Add-member NoteProperty Region $region.Region
+                        if($status){
+                            $obj |Add-member NoteProperty InstanceState $status.instancestate.name
+                            $obj |Add-member NoteProperty Reachability $status.status.status
+                            $obj |Add-member NoteProperty InstanceStatus $status
 
                         }
-                        
+                        else{
+                            $obj |Add-member NoteProperty InstanceState "NoStateFound"
+                            $obj |Add-member NoteProperty Reachability "NoStateFound"
+                            $obj |Add-member NoteProperty InstanceStatus "NoStateFound"
+                        }
+                        $obj |Add-member NoteProperty Instances $ec2.instances
+                        $ec2Array += $obj
+
                     }
-                    catch{}
-                    $regionProgress++
+                    
                 }
+                catch{}
+                $regionProgress++
             }
-            $orgProgress++
+
         }
+    
+
+
         
     $ec2Array
     
